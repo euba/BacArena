77fc3fc79109ba27ad83d428991f7ce7fde55ae4
johannes <johannes@silmaril.(none)>
2013-05-20 20:26:07 +0200

    combined model of movement and diffusion in R/simecol

---
+++ simecol/diffbac.R
@@ -0,0 +1,59 @@
+# Diffusion and movement
+
+n <- 200
+m <- 200
+
+s <- c("M_ac_b","M_akg_b", "M_co2_b", "M_etoh_b", "M_for_b", "M_fum_b", "M_glc_D_b", "M_h2o_b", "M_h_b", "M_lac_D_b","M_o2_b", "M_pi_b", "M_pyr_b", "M_succ_b")
+
+substrat <- lapply(s, function(x, n, m){
+  matrix(runif(n*m), nrow=n, ncol=m)
+}, n=n, m=m)
+names(substrat) <- s
+
+
+diff <- new("rwalkModel",
+            main = function(time, init, parms) {
+              x<- init$sub             
+              # diffusion by mean over neighbourhood
+              x <- lapply(x, function(x){
+                anb <- eightneighbours(x)
+                nb <- neighbours(x)
+                mat <- (anb+x) / (nb+1)
+                return(mat)
+              })
+              #image(x$M_co2_b, zlim=c(0,1))
+              par(mfrow=c(2,2))
+              for(i in 1:4){
+                image(x[[i]], zlim=c(0,1))
+              }
+              #print(x$M_co2_b)
+              #x2 <- init$bac
+              #y <- x2
+              #for (i in 0:(parms$n-1)){
+              #  for(j in 0:(parms$m-1)){
+              #    if(y[i+1,j+1] != 0){
+              #      a <- (i + round(runif(1,-1,1))) %% parms$n 
+              #      b <- (j + round(runif(1,-1,1))) %% parms$m 
+              #      if(x2[a+1,b+1] == 0){
+              #        x2[a+1,b+1] <- 1
+              #        x2[i+1,j+1] <- 0
+                      #print(c(i,j,a,b))
+              #      }
+              #    }
+              #  }
+              #}
+              #image(x2)
+              #print(sum(apply(x2, 1, sum)))
+              #print(x)
+              #return(list(sub=x, x2=x2))
+              #return(list(sub=x, bac=x2))
+              return(list(sub=x))
+            },
+            parms = list(n=n, m=m),
+            times = c(from=1, to=50, by=1),
+            init = list(sub=substrat, bac=matrix(round(runif(n*m)), nrow=n, ncol=m)),
+            solver = "iteration"
+)
+
+sim(diff)
+
