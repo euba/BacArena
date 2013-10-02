e4406a85b86746564603ea44799c092b672720cf
euba <eugen@eugen-Lenovo-B550.(none)>
2013-10-02 16:09:27 +0200

    big ecoli model

--- ecoli_iAF1260.R
+++ ecoli_iAF1260.R
@@ -1,8 +1,8 @@
-ecoli_sbml <- read.sbml("data/ecoli_iAF1260.xml", "_b$")
+ecoli_big_sbml <- read.sbml("data/ecoli_iAF1260.xml", "_b$")
 # last delivered variable is regexpr for filtering external reactions (special handling necessary for equilibrium condition in fba)
 
-ecoli_biomassf <- "R_Biomass_Ecoli_core_N__w_GAM_"
+ecoli_big_biomassf <- "R_Biomass_Ecoli_core_N__w_GAM_"
 
-ecoli_maintenancef <- "R_ATPM"
+ecoli_big_maintenancef <- "R_ATPM"
 
-ecoli_stochmatrix <- ecoli_sbml$stoch
+ecoli_big_stochmatrix <- ecoli_big_sbml$stoch
