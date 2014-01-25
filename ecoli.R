950b6d4f3a425a1dbe95c514f27ee551f99d7d21
euba <eugen.3auer@gmail.com>
2014-01-25 17:39:42 +0100

    changed and cleaned up ecoli.R

--- ecoli.R
+++ ecoli.R
@@ -1,11 +1,4 @@
 mod <- readSBMLmod("data/ecoli_core.xml", bndCond = FALSE)
-findExchReact(mod)
-
-ecoli_biomassf <- "R_Biomass_Ecoli_core_N__w_GAM_"
-
-ecoli_maintenancef <- "R_ATPM"
-
-ecoli_stochmatrix <- ecoli_sbml$stoch
 
 #
 # associate each substrate with an exchange reaction (sbml specific!!)
@@ -31,31 +24,6 @@ sname <- c("acetate","aketoglutarate", "co2", "ethanol", "formiate", "fumarate",
 ecoli_sub_ex <- react_id(findExchReact(mod))
 names(ecoli_sub_ex) <- sname
 
-ecoli_lower_bound <- ecoli_sbml$lb
-ecoli_upper_bound <- ecoli_sbml$ub
-
-ecoli_ngam <- 8.39
-ecoli_gam <- 59.81
-
-obj <- optimizeProb(mod, algorithm = "fba", retOptSol = F, solver = "clpAPI")
-lowbnd(mod)[23] = -20
-optimizeProb(mod, algorithm = "fba", retOptSol = F, solver = "clpAPI")$obj
-findExchReact(mod)
-
-changeBounds(mod, ecoli_sub_ex[intersect(names(ecoli_sub_ex), names(substrat))], lb = -substrat[[x]])
-
-
-sapply(names(substrat), function(x, y, z, mod, substrat, ecoli_sub_ex){
-  rxn <- ecoli_sub_ex[x]
-  if(!is.na(rxn)){
-    mod <<- changeBounds(mod, rxn, lb=substrat[[x]][y,z])
-  }
-}, mod=mod, substrat=substrat, y=2, z=2, ecoli_sub_ex=ecoli_sub_ex)
-
-react_id(findExchReact(mod){
-  ecoli_sub_ex[]
-})
-
 #
 # set lower bounds to current substrat concentration in cell
 #
@@ -64,19 +32,28 @@ react_id(findExchReact(mod){
 #the maximum aerobic glucose utilization rate (10.5 mmol of Glc per g [dryweight] per h), the maximum anaerobic glucose utilization rate (18.5 mmol of Glc per g [dry weight] per h),  
 #the non-growth-associated maintenance requirements (7.6 mmol of ATP per g [dry weight] per h), and the growth-associated maintenance requirements (13 mmol of ATP per g of biomass).
 
-ecoli_set_lower_bound <- function(substrat){
+ecoli_set_lower_bound <- function(substrat){ 
+  #lowbnd(mod) = rep(0, length(lowbnd(mod))) #define growth media
+  sapply(names(substrat), function(x, y, z, substrat, ecoli_sub_ex){
+    rxn <- ecoli_sub_ex[x]
+    if(!is.na(rxn)){
+      mod <<- changeBounds(mod, rxn, lb=-substrat[[x]][y,z])
+    }
+  }, substrat=substrat, y=1, z=1, ecoli_sub_ex=ecoli_sub_ex)
+  mod <- changeBounds(mod, c(ecoli_sub_ex["glucose"], ecoli_sub_ex["o2"]), lb=-c(11, 18.2))
+  
   #
   # lower/upper bound is init in read.sbml because of irreversible reactions => lb=0
   #
-  ecoli_lower_bound <- ecoli_sbml$lb
-  ecoli_upper_bound <- ecoli_sbml$ub
-  ecoli_lower_bound[grep("R_EX", colnames(ecoli_stochmatrix))] <- 0 # define growth media
+  #ecoli_lower_bound <- ecoli_sbml$lb
+  #ecoli_upper_bound <- ecoli_sbml$ub
+  #ecoli_lower_bound[grep("R_EX", colnames(ecoli_stochmatrix))] <- 0 # define growth media
   
   #print(substrat)
-  sapply(names(sapply(substrat, names)), function(x, ecoli_stochmatrix, ecoli_sub_ex, ecoli_lower_bound){
+  #sapply(names(sapply(substrat, names)), function(x, ecoli_stochmatrix, ecoli_sub_ex, ecoli_lower_bound){
     #print(x)
-    if(x %in% names(ecoli_sub_ex)) ecoli_lower_bound[which(colnames(ecoli_stochmatrix)==ecoli_sub_ex[[x]])] <<- - substrat[[x]] # "<<-" is necessary for extern variable modification
-  },ecoli_stochmatrix=ecoli_stochmatrix, ecoli_sub_ex=ecoli_sub_ex, ecoli_lower_bound=ecoli_lower_bound)
+  #  if(x %in% names(ecoli_sub_ex)) ecoli_lower_bound[which(colnames(ecoli_stochmatrix)==ecoli_sub_ex[[x]])] <<- - substrat[[x]] # "<<-" is necessary for extern variable modification
+  #},ecoli_stochmatrix=ecoli_stochmatrix, ecoli_sub_ex=ecoli_sub_ex, ecoli_lower_bound=ecoli_lower_bound)
   
   
   #limit flux exchange rate:
@@ -87,12 +64,12 @@ ecoli_set_lower_bound <- function(substrat){
   #}
   
   #Feist et al 2007:
-  if(substrat[["glucose"]] > 11) ecoli_lower_bound[which(colnames(ecoli_stochmatrix)==ecoli_sub_ex[["glucose"]])] <- -11
-  if(substrat[["o2"]] > 18.2) ecoli_lower_bound[which(colnames(ecoli_stochmatrix)==ecoli_sub_ex[["o2"]])] <- -18.2
+  #if(substrat[["glucose"]] > 11) ecoli_lower_bound[which(colnames(ecoli_stochmatrix)==ecoli_sub_ex[["glucose"]])] <- -11
+  #if(substrat[["o2"]] > 18.2) ecoli_lower_bound[which(colnames(ecoli_stochmatrix)==ecoli_sub_ex[["o2"]])] <- -18.2
   #Orth et al 2011:
-  if(substrat[["lactate"]] > 16) ecoli_lower_bound[which(colnames(ecoli_stochmatrix)==ecoli_sub_ex[["lactate"]])] <- -16
-  if(substrat[["succinate"]] > 16) ecoli_lower_bound[which(colnames(ecoli_stochmatrix)==ecoli_sub_ex[["succinate"]])] <- -16
-  
-  changeBounds(model, react, lb = NULL, ub = NULL)
-  return(ecoli_lower_bound)
+  #if(substrat[["lactate"]] > 16) ecoli_lower_bound[which(colnames(ecoli_stochmatrix)==ecoli_sub_ex[["lactate"]])] <- -16
+  #if(substrat[["succinate"]] > 16) ecoli_lower_bound[which(colnames(ecoli_stochmatrix)==ecoli_sub_ex[["succinate"]])] <- -16
+  #return(ecoli_lower_bound)
 }
+
+optimizeProb(mod, algorithm = "fba", retOptSol = F, solver = "clpAPI")$obj #test objective
