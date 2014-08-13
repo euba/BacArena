2cfd59a418901b47298832f681ff8ffaf1c68b2f
jo <jotech@server.fake>
2014-08-13 15:11:09 +0200

    small test simulation

--- simulation.R
+++ simulation.R
@@ -20,9 +20,9 @@ mod <- model
 specs=list()
 specs[[1]]=mod
 
-pop = Population(specs, specn=rep(50, length(specs)), n=40, m=40)
+pop = Population(specs, specn=rep(1, length(specs)), n=2, m=2)
 
-for(i in 1:50){
+for(i in 1:2){
   moveRand(pop)
   #lapply(pop@media, diffuseNaive)
   for(j in seq_along(pop@media)){
@@ -30,20 +30,21 @@ for(i in 1:50){
   }
   bacs <- pop@orglist
   media <- pop@media
-  for(i in seq_along(bacs)){
-    bac <- bacs[[i]]
+  for(j in seq_along(bacs)){
+    bac <- bacs[[j]]
     for(k in seq_along(media)){
       constrain(bac, media[[k]]@name, lb=-media[[k]]@diffmat[bac@x,bac@y])
-      consume(bac)
+      consume(bac, pop@media[[k]])
     }
+    print(bac@)
     optimizeLP(bac)
-    consume()
   }
   
-  dmat <- pop@media[["EX_glc(e)"]]@diffmat
-  image(dmat)
+  #dmat <- pop@media[["EX_glc(e)"]]@diffmat
+  #image(dmat)
   image(pop2mat(pop))
-  #print(i)
+  print(i)
+  print(pop@media[["EX_glc(e)"]]@diffmat)
   #print(sum(unlist(lapply(pop@orglist, function(x){print(x@growth)}))))
   #print(pop@orgn)
 }
