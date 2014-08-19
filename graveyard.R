2d77b0f957fc4b127a4eeec7d12dad5855549ead
jo <jotech@server.fake>
2014-08-19 12:19:29 +0200

    cleaning stuff

--- graveyard.R
+++ graveyard.R
@@ -1,163 +1,98 @@
-#############################################################################
-############################ C++ graveyard
+#function for converting Arena object into a position/type data.frame of all organisms involved
 
-# Diffusion with correct math model
-src_math_diffusion <- '
-Rcpp::List xlist(A);
-int s = xlist.size();
-int seed2 = as<int>(seed);  
-srand (seed2); /* initialize random seed: */
+setGeneric("pop2dat", function(object){standardGeneric("pop2dat")})
+setMethod("pop2dat", "Arena", function(object){
+  tmp <- lapply(object@orglist, function(x){return(c(x@x, x@y, x@type))})
+  tmp <- t(as.data.frame(tmp))
+  popdat <- data.frame("x"=as.numeric(tmp[,1]), "y"=as.numeric(tmp[,2]), "type"=as.factor(tmp[,3]), row.names=1:nrow(tmp))
+  return(popdat)
+})
 
-for(int k=0; k<s; k++) {
-SEXP ll = xlist[k];
-Rcpp::NumericMatrix y(ll);
+#function for converting Arena object into a position/object data.frame of all organisms involved
 
-int n = y.ncol();
-int m = y.nrow();
-A = 1
+setGeneric("pop2dat2", function(object){standardGeneric("pop2dat2")})
+setMethod("pop2dat2", "Arena", function(object){
+  tmp <- lapply(object@orglist, function(x){return(c(x@x, x@y, x))})
+  tmp <- t(as.data.frame(tmp))
+  popdat <- data.frame("x"=as.numeric(tmp[,1]), "y"=as.numeric(tmp[,2]), "type"=as.factor(tmp[,3]), row.names=1:nrow(tmp))
+  return(popdat)
+})
 
+#function for converting Arena object into a presence/absence matrix of all organisms involved
 
-
-}
-'
-
-
-
-# Diffusion by mean with lowest neighbour
-# synchron updating!
-#
-src_diffusion3 <- '
-Rcpp::List xlist(A);
-int n = xlist.size();
-for(int k=0; k<n; k++) {
-SEXP ll = xlist[k];
-Rcpp::NumericMatrix y(ll);
-//copy matrix to diffuse successive
-//Rcpp::NumericMatrix tmp = Rcpp::clone(y);
-//std::cout << y(1,1);
-int n = y.ncol();
-int m = y.nrow();
-for(int i=0; i<n; i++){
-for(int j=0; j<m; j++){
-double min = y(i,j);
-std::vector<std::pair<int,int> > list_min; //list containing minima
-for(int l=-1; l<=1; l++){
-for(int o=-1; o<=1; o++){
-int pos_i = i + l;
-int pos_j = j + o;
-if(pos_i>=0 && pos_i<n && pos_j>=0 && pos_j<m){ //boundary check
-if(y(pos_i,pos_j) < min) {
-min = y(pos_i,pos_j);
-list_min.clear(); // clear list with old minima
-std::pair <int,int> new_min(pos_i,pos_j);
-list_min.push_back(new_min);
-}
-else if(y(pos_i,pos_j) == min) {
-std::pair <int,int> new_min(pos_i,pos_j);
-list_min.push_back(new_min);
-}
-}
-}
-}
-std::random_shuffle ( list_min.begin(), list_min.end() ); //randomize order
-// choose randomly one of the found minima
-
-int min_i = list_min.front().first;
-int min_j = list_min.front().second;
-double m = (y(i,j) + y(min_i,min_j)) / 2.0;
-
-y(i,j) = m;
-y(min_i, min_j) = m;
-}
-}
-}
-//return(A);
-'
+setGeneric("pop2mat", function(object){standardGeneric("pop2mat")})
+setMethod("pop2mat", "Arena", function(object){
+  popdat <- pop2dat(object)
+  popmat <- matrix(0, object@n, object@m)
+  apply(popdat, 1, function(x, types){
+    popmat[as.numeric(x[1]), as.numeric(x[2])] <<- which(types==x[3])
+  }, types=levels(popdat$type))
+  return(popmat)
+})
 
 
+#function for converting Arena object into a presence/absence matrix of all individuals involved
 
-# Diffusion by moving average
-src_diffusion2 <- '
-Rcpp::List xlist(A);
-int n = xlist.size();
-for(int k=0; k<n; k++) {
-SEXP ll = xlist[k];
-Rcpp::NumericMatrix y(ll);
-//copy matrix to diffuse sucsessive
-Rcpp::NumericMatrix tmp = Rcpp::clone(y);
-//std::cout << y(1,1);
-int n = tmp.ncol();
-int m = tmp.nrow();
-for(int i=0; i<n; i++){
-for(int j=0; j<m; j++){
-//get neighbours
-int sum = 0;
-int nb = 0;
-for(int l=-1; l<=1; l++){
-for(int o=-1; o<=1; o++){
-int pos_i = i + l;
-int pos_j = j + o;
-if(pos_i>=0 && pos_i<n && pos_j>=0 && pos_j<m){
-nb++;
-sum += tmp(pos_i, pos_j);
-}
-}
-}
-y(i,j) = (double)sum/(double)nb;
-}
-}
-}
-//return(A);
-'
+setGeneric("pop2imat", function(object){standardGeneric("pop2imat")})
+setMethod("pop2imat", "Arena", function(object){
+  popdat <- pop2dat(object)
+  popmat <- matrix(0, object@n, object@m)
+  for(i in 1:nrow(popdat)){
+    popmat[popdat[i,]$x, popdat[i,]$y] <- i
+  }
+  return(popmat)
+})
 
-src_movement <- '
-Rcpp::NumericMatrix tmp(input_matrix);
-Rcpp::DataFrame l(input_frame);
-Rcpp::IntegerVector x = l["x"];
-Rcpp::IntegerVector y = l["y"];
-int seed2 = as<int>(seed);  
 
-x=x-1;  // index in c++ starts at 0
-y=y-1;
+#function for random walk (movement) of the whole Arena through the grid space
 
-/* initialize random seed: */
-srand (seed2);
-
-int n = tmp.nrow();
-int m = tmp.ncol();
-
-//std::cout << l.length();
-//std::cout << as<std::vector>(l(0))[1] << std::endl;
-
-std::set<std::pair<int,int> > s;
-for(int i=0; i < x.size(); i++){
-s.insert(std::make_pair(x(i), y(i)));
-}
-
-for(int i=0; i < x.size(); i++){
-int a = (x(i) + rand() % 3 - 1);
-int b = (y(i) + rand() % 3 - 1);
-if(a == -1) a = n-1;
-if(b == -1) b = m-1;
-if(a == n) a = 0;
-if(b == m) b = 0;
-
-if(s.find(std::make_pair(a,b)) == s.end()){ // if empty go for it!
-tmp(a,b) = 1;
-s.insert(std::make_pair(a,b));
-s.erase(std::make_pair(x(i),y(i)));
-//std::cout << "move: (" << x(i)-1 << "," << y(i)-1 << ") -> (" << a+1 << "," << b+1 << ")" << std::endl;
-x(i) = a; 
-y(i) = b;
-}
-else tmp(x(i), y(i)) = 1;
-}  
-
-x=x+1; // index in R starts at 1
-y=y+1;
-
-Rcpp::DataFrame new_l = Rcpp::DataFrame::create(Rcpp::Named("x")=x, Rcpp::Named("y")=y, Rcpp::Named("type")=l[2], Rcpp::Named("growth")=l[3]);
-return(Rcpp::List::create(Rcpp::Named("df")=new_l, Rcpp::Named("matrix")=tmp));
-'
-
-movement <- cxxfunction(signature(input_matrix = "matrix", input_frame = "data.frame", seed = "integer"), body = src_movement, plugin="Rcpp")
+setGeneric("moveRand", function(object){standardGeneric("moveRand")})
+setMethod("moveRand", "Arena", function(object){
+  n <- object@n
+  m <- object@m
+  bmat <- pop2imat(object)
+  bmatn <- matrix(NA, nrow=n+2, ncol=m+2) #define environment with boundary conditions
+  bmatn[2:(n+1), 2:(m+1)] <- bmat #put the values into the environment
+  bdat <- object@orglist
+  for(i in seq_along(bdat)){
+    bmatn[2:(n+1), 2:(m+1)] <- bmat #put the values into the environment
+    ic = bdat[[i]]@x
+    jc = bdat[[i]]@y
+    neighbours <- c(bmatn[ic,jc], 
+                    bmatn[ic+1,jc], 
+                    bmatn[ic+2,jc], 
+                    bmatn[ic+2,jc+1],
+                    bmatn[ic+2,jc+2], 
+                    bmatn[ic+1,jc+2],
+                    bmatn[ic,jc+2],
+                    bmatn[ic,jc+1])
+    neighbours = ifelse(is.na(neighbours),1,neighbours)
+    pos <- which(neighbours==0)
+    if(length(pos) >= 1){
+      if(length(pos)!=1){
+        pos = sample(pos, 1)
+      }
+      switch(pos,
+{bmat[ic-1,jc-1] <- bmat[ic,jc]; bmat[ic,jc] <- 0},
+{bmat[ic,jc-1] <- bmat[ic,jc]; bmat[ic,jc] <- 0},
+{bmat[ic+1,jc-1] <- bmat[ic,jc]; bmat[ic,jc] <- 0},
+{bmat[ic+1,jc] <- bmat[ic,jc]; bmat[ic,jc] <- 0},
+{bmat[ic+1,jc+1] <- bmat[ic,jc]; bmat[ic,jc] <- 0},
+{bmat[ic,jc+1] <- bmat[ic,jc]; bmat[ic,jc] <- 0},
+{bmat[ic-1,jc+1] <- bmat[ic,jc]; bmat[ic,jc] <- 0},
+{bmat[ic-1,jc] <- bmat[ic,jc]; bmat[ic,jc] <- 0})
+    }else{
+      if(length(pos) == 0){
+        next
+      }
+    }
+newpos <- which(bmat==i, arr.ind=T)
+tmp <- object@occmat[[bdat[[i]]@x,bdat[[i]]@y]]
+object@occmat[[bdat[[i]]@x,bdat[[i]]@y]] <- 0
+bdat[[i]]@x <- newpos[1]
+bdat[[i]]@y <- newpos[2]
+object@occmat[[bdat[[i]]@x,bdat[[i]]@y]] <- tmp
+  }
+eval.parent(substitute(object@orglist <- bdat))
+eval.parent(substitute(object@occmat <- object@occmat))
+})
