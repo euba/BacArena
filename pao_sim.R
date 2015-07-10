8e44cd8ca2fc2649981073296f0281e6b93d2a0d
Eugen Bauer <eugen.bauer@uni.lu>
2015-07-10 19:12:02 +0200

    chemotaxis in pao_sim

--- pao_sim.R
+++ pao_sim.R
@@ -1,5 +1,7 @@
+setwd("P:/BACARENA/Comparison/MatNet/P_aeruginosa/")
+
 library(sybilSBML)
-model = readSBMLmod("P:/BACARENA/Comparison/MatNet/P_aeruginosa/POA_test.xml")
+model = readSBMLmod("P:/BACARENA/Comparison/MatNet/P_aeruginosa/modelPOA.xml")
 
 
 set.seed(5000)
@@ -7,16 +9,17 @@ set.seed(5000)
 # if cellMass >= 2
 # cellDeathThreshold = 0
 model@lowbnd[grep("EX",model@react_id)]
-model = changeBounds(model,model@react_id[grep("EX",model@react_id)],lb=-10)
+modelP = changeBounds(model,model@react_id[grep("EX",model@react_id)],lb=-10)
 
-bace1 = Bac(model=model, deathrate=0.5, duplirate=1, growthlimit=0.05, growtype="exponential",
-            speed=1, type="PAO", lyse=T)
+
+bace1 = Bac(model=modelP, deathrate=0.05, duplirate=1, growthlimit=0.05, growtype="exponential",
+            speed=3, type="PAO", lyse=F,chem="EX_EC0007")
 arena = Arena(n=100, m=100, stir=F, tstep=0.5)
-addOrg(arena, bace1, amount=100, x=1:100, y=rep(1,100))
-addSubs(arena, smax=20, difunc="cpp", difspeed=1)
+addOrg(arena, bace1, amount=22, x=c(10:20,80:90), y=rep(1,22))
+addSubs(arena, smax=50, difunc="cpp", difspeed=1,
+        mediac=model@react_id[grep("EX",model@react_id)][which(model@lowbnd[grep("EX",model@react_id)] < -1)])
 
 print(system.time(evalsim <- simEnv(arena, time=50)))
-
 
 library(animation)
 oopts = ani.options(ffmpeg = "C:/ffmpeg.exe")
@@ -24,3 +27,10 @@ saveVideo({
   ani.options(interval = 0.5)
   evalArena(evalsim, phencol=T, plot_items=c('population','EX_EC0007'))
 },video.name = "PAO_pop_phen_bio.avi", other.opts = "-b 600k")
+
+library(animation)
+oopts = ani.options(ffmpeg = "C:/ffmpeg.exe")
+saveVideo({
+  ani.options(interval = 0.5)
+  evalArena(evalsim, phencol=T)
+},video.name = "PAO_pop4.avi", other.opts = "-b 600k")
