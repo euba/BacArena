7007e411080c1c45529fab6ea3f04798c566ffbf
jo <jotech@users.noreply.github.com>
2015-11-13 18:38:23 +0100

    implemented diffusion+advection

--- examples/diff.R
+++ examples/diff.R
@@ -14,22 +14,23 @@ source(file="R/Organism.R")
 source(file="R/Stuff.R")
 
 
-arena = Arena(n=100, m=100, stir=F)
-show(arena)
+arena = Arena(n=100, m=100, stir=F, Lx=10, Ly=10)
+#show(arena)
 data(Ec_core)
-bac = Bac(model=Ec_core, deathrate=0.0, duplirate=1, growthlimit=0.05, growtype="exponential",
-          speed=0, type="ecore", lyse=T)
+bac = Bac(model=Ec_core, growtype="exponential",
+          speed=0, deathrate=0, type="ecore", lyse=F)
 addOrg(arena, bac, amount=1, x=1, y=1)
 addSubs(arena, smax=0, difunc="pde", difspeed=1)
 arena@media$`EX_co2(e)`@diffmat[ceiling(arena@n/2),ceiling(arena@m/2)] <- 100
 arena@media$`EX_co2(e)`@difspeed=0.5
+arena@media$`EX_co2(e)`@pde="AdvecDiff2d"
 sim <- simEnv(arena, time=8)
-sim@medlist[[2]]$`EX_co2(e)`[50]
+#sim@medlist[[2]]$`EX_co2(e)`[50]
 out <- lapply(sim@medlist, function(x){matrix(x$`EX_co2(e)`, nrow=arena@n)})
 par(mfrow = c(ceiling(sqrt(length(out))), ceiling(sqrt(length(out)))))
 for(i in 1:length(out)){image(out[[i]], main=paste("time",i))}
 par(mfrow=c(1,1))
-
+plotCurves2(sim)
 
 
 x=c(10*10, 25*25, 51*51, 61*61, 71*71, 81*81, 91*91, 101*101)
@@ -41,7 +42,6 @@ plot(x,y)
 abline(coef(lm))
 abline(coef=c(0, lm$coefficients[2]))
 
-line())
 
 co2_dat <- c(evalArena())
 evalArena(sim, plot_items = "EX_co2(e)", time=2)
