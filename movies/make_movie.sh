fcd4c6ff763b895c441cb9f7ab4ad05543a33449
jo <jotech@users.noreply.github.com>
2015-11-24 10:24:47 +0100

    cleaning up

--- movies/make_movie.sh
+++
@@ -1,8 +0,0 @@
-mkdir temp
-cp *.jpg temp/.
-mogrify -resize 800x800  temp/*.jpg
-convert temp/*.jpg -delay 10 -morph 10 temp/%05d.jpeg
-ffmpeg -r 25 -qscale 2  -i temp/%05d.jpeg output.mp4
-#rm -R temp
-
-
