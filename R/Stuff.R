6a79f3a054d732e13251d9163d3773540e31d9af
jo <jotech@users.noreply.github.com>
2016-04-10 23:01:24 +0200

    specified reactran method call

--- R/Stuff.R
+++ R/Stuff.R
@@ -36,7 +36,7 @@ Diff2d <- function (t, y, parms){
   # geometry values are in parms
   with (as.list(parms), {
     CONC  <- matrix(nrow = gridgeometry.grid2D$x.N, ncol = gridgeometry.grid2D$y.N, data = y)
-    dCONC <- tran.2D(CONC, grid = gridgeometry.grid2D, D.grid = diffgeometry.Dgrid)$dC
+    dCONC <- ReacTran::tran.2D(CONC, grid = gridgeometry.grid2D, D.grid = diffgeometry.Dgrid)$dC
     return (list(dCONC))
   })
 }
