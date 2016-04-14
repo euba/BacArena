e245907c2420ef1b4597959609d5b8da6ad585db
jo <jotech@users.noreply.github.com>
2016-04-14 18:42:15 +0200

    fixed+extended findInArena
    - wrong object ref
    - find only subtances or reactions flag

--- R/Arena.R
+++ R/Arena.R
@@ -1159,32 +1159,36 @@ setMethod("dat2mat", "Arena", function(object){
 #'
 #' @param object An object of class Arena.
 #' @param pattern A pattern for searching
+#' @param search_rea Only search for reactions
+#' @param search_sub Only search for substances 
 #' @examples
 #' data(Ec_core)
 #' bac <- Bac(Ec_core)
 #' arena <- Arena(n=20,m=20)
 #' addOrg(arena,bac,amount=10)
 #' findInArena(arena, "acetate")
-setGeneric("findInArena", function(object, pattern){standardGeneric("findInArena")})
+setGeneric("findInArena", function(object, pattern, search_rea=TRUE, search_sub=TRUE){standardGeneric("findInArena")})
 #' @export
 #' @rdname findInArena
-setMethod("findInArena", "Arena", function(object, pattern){
-  res_id <- grep(x=object@mediac, pattern=pattern, ignore.case = TRUE)
-  print(object@mediac[res_id])
-  
-  res_name <- grep(x=names(object@mediac), pattern=pattern, ignore.case = TRUE)
-  print(object@mediac[res_name])
-
-  for(i in 1:length(arena@models)){
-    model = arena@models[[i]]
-    cat(paste0("\n\n", i, ". ", model@mod_desc, model@mod_name))
-    res_rea_id   <- grep(x=model@react_id,   pattern=pattern, ignore.case = TRUE)
-    print(paste(model@react_id[res_rea_id], model@react_name[res_rea_id]))
-    
-    res_rea_name <- grep(x=model@react_name, pattern=pattern, ignore.case = TRUE)
-    print(paste(model@react_id[res_rea_name], model@react_name[res_rea_name]))
+setMethod("findInArena", "Arena", function(object, pattern, search_rea=TRUE, search_sub=TRUE){
+  if(search_sub){
+    res_id <- grep(x=object@mediac, pattern=pattern, ignore.case = TRUE)
+    print(object@mediac[res_id])
+    res_name <- grep(x=names(object@mediac), pattern=pattern, ignore.case = TRUE)
+    print(object@mediac[res_name])
   }
   
+  if(search_rea){
+    for(i in 1:length(arena@models)){
+      model = arena@models[[i]]
+      cat(paste0("\n\n", i, ". ", model@mod_desc, model@mod_name))
+      res_rea_id   <- grep(x=model@react_id,   pattern=pattern, ignore.case = TRUE)
+      print(paste(model@react_id[res_rea_id], model@react_name[res_rea_id]))
+      
+      res_rea_name <- grep(x=model@react_name, pattern=pattern, ignore.case = TRUE)
+      print(paste(model@react_id[res_rea_name], model@react_name[res_rea_name]))
+    }
+  }
 })
 
 
