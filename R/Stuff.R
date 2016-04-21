204284e9dc76fda1f650c8c7b79584ad772199fc
Eugen Bauer <eugen.bauer@uni.lu>
2016-04-21 18:20:38 +0200

    Added features for growth plot of replicates.

--- R/Stuff.R
+++ R/Stuff.R
@@ -230,7 +230,7 @@ plotSubCurve <-function(simlist, mediac=NULL){
 #' 
 #' @param simlist A list of simulations (eval objects).
 #'
-plotGrowthCurve <-function(simlist){
+plotGrowthCurve <-function(simlist, bcol){
   if(length(simlist) < 1 | !all(lapply(simlist, class) == "Eval") == TRUE) stop("Simlist is invalid.")
   
   all_df <- data.frame()
@@ -246,8 +246,27 @@ plotGrowthCurve <-function(simlist){
   }
   
   #ggplot(all_df, aes(color=Var1, y=value, x=Var2)) + geom_point()
-  q<-ggplot(all_df, aes(color=Var1, y=value, x=Var2)) + #stat_smooth(se = FALSE) + 
-    stat_summary(geom="ribbon", fun.ymin="lsd", fun.ymax="usd", aes(fill=Var1), alpha=0.3)
+  q<-ggplot(all_df, aes(color=Var1, y=value, x=Var2-1)) + #stat_smooth(se = FALSE) + 
+    stat_summary(geom="ribbon", fun.ymin="lsd", fun.ymax="usd", aes(fill=Var1), alpha=0.3) +
+    scale_fill_manual(values=bcol) +
+    scale_color_manual(values=bcol) +
+    xlab("Time in h") +
+    ylab("Number of individuals") +
+    theme_bw(base_size = 30) +
+    theme(#legend.position='none',
+      legend.text=element_text(size=14),
+      legend.key=element_blank(),
+      legend.title = element_blank(),
+      axis.text.x = element_text(size=20),
+      axis.text.y = element_text(size=20),
+      axis.title.y = element_text(size=30,vjust=0.5),
+      #panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.border = element_rect(colour='black',size=2),
+      axis.ticks = element_line(size=1,color='black'),
+      plot.title = element_text(size=20)) #15x5           # Position legend in bottom right
+  return(q)
+  
 }
 
 
