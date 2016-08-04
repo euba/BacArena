35ec3a7440e438b1e4d51502906ffef45ebb6a53
jo <jotech@users.noreply.github.com>
2016-08-04 16:02:59 +0200

    added new normalized bar plot to plotGrowthCurve()

--- R/Stuff.R
+++ R/Stuff.R
@@ -305,8 +305,10 @@ plotGrowthCurve <-function(simlist, time=c(NULL,NULL)){
   q3<-ggplot2::ggplot(all_df, ggplot2::aes_string(color="species", y="value", x="time")) +
     ggplot2::stat_summary(fun.y = mean, geom="line", size=1) + 
     ggplot2::xlab("Time in h") + ggplot2::ylab("Number of individuals")
+  q4 <- ggplot2::ggplot(all_df, ggplot2::aes_string("time", "value")) +
+    ggplot2::stat_summary(fun.y = mean, geom="bar", width=1, position="fill", ggplot2::aes_string(fill="species"))
   if(length(cap)!=0){q3 <- q3 + ggplot2::geom_vline(xintercept=min(cap))}
-  if(length(plot_list)==0) plot_list <- q3 else plot_list[[length(plot_list)+1]] <- q3
+  if(length(plot_list)==0) plot_list <- list(q3,q4) else {plot_list[[length(plot_list)+1]] <- q3; plot_list[[length(plot_list)+2]] <- q4}
   
   return(plot_list)
 }
