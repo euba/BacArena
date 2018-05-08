32bba346d32285dae523c9d8136e79535bf4d462
GMaringos <maringos@users.noreply.github.com>
2018-05-08 23:47:02 +0200

    Hiding the % growth in the start of iteration

--- R/Arena.R
+++ R/Arena.R
@@ -859,7 +859,7 @@ setMethod("simEnv", "Arena", function(object, time, lrw=NULL, continue=FALSE, re
       old_biomass<-biomass_stat; biomass_stat <- sapply(seq_along(arena@specs), function(x){sum(arena@orgdat$biomass[which(arena@orgdat$type==x)])})
       org_stat <- cbind(org_stat, biomass_stat); rownames(org_stat) <- names(arena@specs); colnames(org_stat) <- c("count", "biomass")
       if(verbose) print(org_stat)}}
-    if(verbose & i!=1) print(org_stat)
+    if(verbose & i!=1) print(org_stat[,1:2])
     arena@mflux <- lapply(arena@mflux, function(x){numeric(length(x))}) # empty mflux pool
     arena@shadow <-lapply(arena@shadow, function(x){numeric(length(x))}) # empty shadow pool
     if(nrow(arena@orgdat) > 0){ # if there are organisms left
