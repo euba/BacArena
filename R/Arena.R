2745d7f6da67114b71a8bffc38a7dc4455846209
maringos <g.marinos@iem.uni-kiel.de>
2018-05-09 13:30:04 +0200

    using as.data.frame to be consistant with https://github.com/maringos/BacArena/commit/292eabbc4231a39b6cbf75106c6a035431e3342b

--- R/Arena.R
+++ R/Arena.R
@@ -858,7 +858,7 @@ setMethod("simEnv", "Arena", function(object, time, lrw=NULL, continue=FALSE, re
     if(length(arena@specs) > 0){
       old_biomass<-biomass_stat; biomass_stat <- sapply(seq_along(arena@specs), function(x){sum(arena@orgdat$biomass[which(arena@orgdat$type==x)])})
       org_stat <- cbind(org_stat, biomass_stat); rownames(org_stat) <- names(arena@specs); colnames(org_stat) <- c("count", "biomass")
-      if(verbose) print(org_stat)}}
+      if(verbose) print(as.data.frame(org_stat))}}
     if(verbose & i!=1) print(as.data.frame(org_stat)[,1:2])
     arena@mflux <- lapply(arena@mflux, function(x){numeric(length(x))}) # empty mflux pool
     arena@shadow <-lapply(arena@shadow, function(x){numeric(length(x))}) # empty shadow pool
